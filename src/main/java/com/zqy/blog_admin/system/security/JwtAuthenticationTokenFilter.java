120be148a8369bb17826b6cac32777300e0217e8
zhangyuanyuan <2264371622@qq.com>
2022-10-08 17:12:44 +0800

    README.md

--- src/main/java/com/zqy/blog_admin/system/security/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/zqy/blog_admin/system/security/JwtAuthenticationTokenFilter.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * 这个filter暂时没有使用到，但是在做spring Security 认证授权过滤器的时候，也有很多采用OncePerRequestFilter的
+ */
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     
     @Override
